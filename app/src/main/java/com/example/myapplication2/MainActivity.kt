05c9a5761c4c547ac2b432cb851d49cde8af890e
Avinash Kumar Singh <aksdav05@gmail.com>
2024-06-15 01:17:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ai.client.generativeai.GenerativeModel
+import kotlinx.coroutines.runBlocking
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ePrompt = findViewById<EditText>(R.id.ePrompt)
+        val submit = findViewById<Button>(R.id.button)
+        val eResponse = findViewById<TextView>(R.id.textView)
+
+        submit.setOnClickListener {
+            val prompt = ePrompt.text.toString()
+            val generativeModel = GenerativeModel(
+                modelName = "gemini-pro",
+                apiKey = "AIzaSyAzUWURUi161oBsbJpIvCHAmLQJIfk_PFM"
+            )
+
+            runBlocking {
+                val response = generativeModel.generateContent(prompt)
+                eResponse.text = response.text
+
+
+            }
+
+        }
+    }
+}
